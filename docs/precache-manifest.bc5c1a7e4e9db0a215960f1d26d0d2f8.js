a49d7882e8e690ac65c337785028270ccca9dd9f
Wajeeh Anwar <wajeeh.anwar@gmail.com>
2020-02-09 03:23:34 -0500

    update demo

---
+++ docs/precache-manifest.bc5c1a7e4e9db0a215960f1d26d0d2f8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4418de3badde4783dcf70493cefd557",
+    "url": "/react-dashboard/index.html"
+  },
+  {
+    "revision": "dbc726e7f1b717fb0399",
+    "url": "/react-dashboard/static/css/main.981ab5df.chunk.css"
+  },
+  {
+    "revision": "0b4828214ed879eed34f",
+    "url": "/react-dashboard/static/js/2.cb9959a2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-dashboard/static/js/2.cb9959a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbc726e7f1b717fb0399",
+    "url": "/react-dashboard/static/js/main.85079754.chunk.js"
+  },
+  {
+    "revision": "ae92646b68bd8c8bade9",
+    "url": "/react-dashboard/static/js/runtime-main.51bc6bee.js"
+  },
+  {
+    "revision": "ec3b8eac7e5f946d6a5d5114a0cf4f21",
+    "url": "/react-dashboard/static/media/bg_1.ec3b8eac.jpg"
+  },
+  {
+    "revision": "f7625e3c4bc3ac2e25cfccb3bb5aebee",
+    "url": "/react-dashboard/static/media/bg_1_thumb.f7625e3c.jpg"
+  },
+  {
+    "revision": "a6753a4e2a5138f875f50e8b5940f4fc",
+    "url": "/react-dashboard/static/media/bg_2.a6753a4e.jpg"
+  },
+  {
+    "revision": "65973e5f248071361d5991cea98b12ed",
+    "url": "/react-dashboard/static/media/bg_2_thumb.65973e5f.jpg"
+  },
+  {
+    "revision": "cfb2493cf9d8d67d61e7c5a9de4d26e8",
+    "url": "/react-dashboard/static/media/bg_3.cfb2493c.jpg"
+  },
+  {
+    "revision": "e5e88cd63066fe06e014f118be34a0e0",
+    "url": "/react-dashboard/static/media/bg_3_thumb.e5e88cd6.jpg"
+  },
+  {
+    "revision": "9b6356269430af799e7124f4e8d6646d",
+    "url": "/react-dashboard/static/media/bg_4.9b635626.png"
+  },
+  {
+    "revision": "4300339bb22ac0a20456f3788bcfdb69",
+    "url": "/react-dashboard/static/media/bg_4_thumb.4300339b.png"
+  },
+  {
+    "revision": "b9643395eba99b0a0427e79301c04068",
+    "url": "/react-dashboard/static/media/bg_5.b9643395.png"
+  },
+  {
+    "revision": "4e49be4d186b4018cf84074db67f8358",
+    "url": "/react-dashboard/static/media/bg_5_thumb.4e49be4d.png"
+  },
+  {
+    "revision": "eef391c456bab92043d7a98d679841a5",
+    "url": "/react-dashboard/static/media/bg_6.eef391c4.png"
+  },
+  {
+    "revision": "22206c35552229a36c99d1e36a4b3124",
+    "url": "/react-dashboard/static/media/bg_6_thumb.22206c35.png"
+  }
+]);
